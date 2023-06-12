0207826ff8a36109f438df91d390ef967a7b0de0
bitnaleeeee <bitnaleeeee@gmail.com>
2023-06-12 22:34:02 +0900

    nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -60,6 +60,62 @@ function Nav() {
           </div>
         </div>
       </div>
+      <div className="daum_service">
+        <div className="inner_service">
+          <div className="menu_service">
+            <ul>
+              <a href="#">
+                <li className="cafe">카페</li>
+              </a>
+              <a href="#">
+                <li className="email">메일</li>
+              </a>
+              <a href="#">
+                <li className="news">뉴스</li>
+              </a>
+              <a href="#">
+                <li className="map">지도</li>
+              </a>
+              <a href="#">
+                <li className="insurance">증권</li>
+              </a>
+              <a href="#">
+                <li className="shopping">쇼핑</li>
+              </a>
+              <a href="#">
+                <li className="kakaotv">카카오TV</li>
+              </a>
+              <a href="#">
+                <li className="webtoon">웹툰</li>
+              </a>
+              <a href="#">
+                <li className="calender">캘린더</li>
+              </a>
+              <a href="#">
+                <li className="story">스토리</li>
+              </a>
+              <a href="#">
+                <li className="dictionary">사전</li>
+              </a>
+              <a href="#">
+                <li className="game">게임</li>
+              </a>
+              <a href="#">
+                <li className="together">같이가치</li>
+              </a>
+            </ul>
+            <a href="#" className="link_more">
+              더보기
+            </a>
+            <div className="today_inform">
+              <span className="todaydue">06. 12. (월)</span>
+              <span className="todaylocation">서울시</span>
+              <span className="todaytemper">22</span>
+            </div>
+          </div>
+          <div className="daum_today"></div>
+        </div>
+      </div>
     </div>
   );
 }
